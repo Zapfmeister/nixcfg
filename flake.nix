6541d9b5c2655286d55fefb5376ba02ab6843793
Zapfmeister <zapfmeister@gmail.com>
2025-04-24 07:47:46 +0200

    Add dotfiles for nvim and bat

--- flake.nix
+++ flake.nix
@@ -17,39 +17,47 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-  };
 
-  outputs = {
-    self,
-    home-manager,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    packages =
-      forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    overlays = import ./overlays {inherit inputs;};
-    nixosConfigurations = {
-      proxvm = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [./hosts/proxvm];
+    dotfiles = {
+      url = "git+https://github.com/Zapfmeister/nixdotfiles.git";
+      flake = false;
+    }
       };
-    };
-    homeConfigurations = {
-      "gzapf@gzapf" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [./home/gzapf/proxvm.nix];
+
+    outputs =
+      { self
+      , dotfiles
+      , home-manager
+      , nixpkgs
+      , ...
+      } @ inputs:
+      let
+        inherit (self) outputs;
+        systems = [
+          "aarch64-linux"
+          "i686-linux"
+          "x86_64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+        ];
+        forAllSystems = nixpkgs.lib.genAttrs systems;
+      in
+      {
+        packages =
+          forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+        overlays = import ./overlays { inherit inputs; };
+        nixosConfigurations = {
+          proxvm = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs outputs; };
+            modules = [ ./hosts/proxvm ];
+          };
+        };
+        homeConfigurations = {
+          "gzapf@gzapf" = home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages."x86_64-linux";
+            extraSpecialArgs = { inherit inputs outputs; };
+            modules = [ ./home/gzapf/proxvm.nix ];
+          };
+        };
       };
-    };
-  };
-}
+  }
