ae9a241c9a1cf977be305f585d8ff4d040472d86
Zapfmeister <zapfmeister@gmail.com>
2025-04-24 14:23:12 +0200

    Update stable channel to 24.11

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         inputs.nixpkgs.follows = "nixpkgs";
       };
       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
       disko = {
         url = "github:nix-community/disko";
